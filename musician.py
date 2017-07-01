6319763021bffd38fe884ed1a80651317964cd4f
pmalexander <pmalexander5@gmail.com>
2017-07-01 12:42:28 -0400

    Create musician.py

---
+++ musician.py
@@ -0,0 +1,34 @@
+class Musician(object):
+    def __init__(self, sounds):
+        self.sounds = sounds
+
+    def solo(self, length):
+        for i in range(length):
+            print(self.sounds[i % len(self.sounds)], end=" ")
+        print()
+        
+class Bassist(Musician): # The Musician class is the parent of the Bassist class
+    def __init__(self):
+        # Call the __init__ method of the parent class
+        super().__init__(["Twang", "Thrumb", "Bling"])
+
+class Guitarist(Musician):
+    def __init__(self):
+        super().__init__(["Boink", "Bow", "Boom"])
+
+    def tune(self):
+        print("Be with you in a moment")
+        print("Twoning, sproing, splang")
+
+class Drummer(Musician)
+    def _init_(self):
+        super().__init__(["Badum", "Tish", "Crash"])
+        
+    count = 0 
+    while count a < 4
+    count = count + 1
+    
+david = Guitarist()
+david.solo(6)
+
+kenneth = Drummer()
