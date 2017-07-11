467c33a5ee542830f476730c1ca3db0e232c383f
pmalexander <pmalexander5@gmail.com>
2017-07-10 23:47:59 -0400

    Update musician.py

--- musician.py
+++ musician.py
@@ -1,141 +1,4 @@
-ver 1.0 (defunct)
-
-class Musician(object):
-    def __init__(self, sounds):
-        self.sounds = sounds
-
-    def solo(self, length):
-        for i in range(length):
-            print(self.sounds[i % len(self.sounds)], end=" ")
-        print()
-        
-class Bassist(Musician): # The Musician class is the parent of the Bassist class
-    def __init__(self):
-        # Call the __init__ method of the parent class
-        super().__init__(["Twang", "Thrumb", "Bling"])
-
-class Guitarist(Musician):
-    def __init__(self):
-        super().__init__(["Boink", "Bow", "Boom"])
-
-    def tune(self):
-        print("Be with you in a moment")
-        print("Twoning, sproing, splang")
-
-class Drummer(Musician)
-    def _init_(self):
-        super().__init__(["Badum", "Tish", "Crash"])
-        
-    count = 0 
-    while count a < 4
-    count = count + 1
-    
-david = Guitarist()
-david.solo(6)
-
-kenneth = Drummer()
-
-ver 2.0 (in progress)
-
 import random
-
-bandmates = {
-    "Kenneth": "Bangs a good drum, when he's sober. You sure?", 
-    "David": "Rocks the bass well, just gotta watch that temper. Cool?",
-    "Derek": "The staple of every band, he was the only one to bother showing up though. Yes?",
-    "Josie": "She can sing your head off, but can get carried away. You good?",
-}
-
-combust_chance = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-class Musician(object):
-    def __init__(self, sounds):
-        self.sounds = sounds
-
-    def solo(self, length):
-        for i in range(length):
-            print(self.sounds[i % len(self.sounds)], end=" ")
-        print()
-        
-class Bassist(Musician): # The Musician class is the parent of the Bassist class
-    def __init__(self):
-        # Call the __init__ method of the parent class
-        super().__init__(["Twang", "Thrumb", "Bling"])
-
-class Guitarist(Musician): # The Musician class is the parent of the Guitarist class
-    def __init__(self):
-        super().__init__(["Boink", "Bow", "Boom"])
-
-    def tune(self):
-        print("Be with you in a moment")
-        print("Twoning, sproing, splang")
-        
-class Vocalist(Musician): # The Musician class is the parent of the Vocalist class
-    def __init__(self):
-        super().__init__(["Oooh", "Ahhhh", "Eeeeee"])
-        
-    def tune(self):
-        print("Have to clear my throat.")
-        print("Do", "Re", "Mi",)    
-        
-class Drummer(Musician): # The Musician class is the parent of the Drummer class
-    def __init__(self):
-        super().__init__(["Badum", "Tish", "Crash"])
-    
-    def tune(self):
-        print("Let me key things up first...")
-        print("Smish, Smash, Brrt")
-        
-    def combust(self):
-        for n in combust_chance:
-            random.choice(n)
-                if n = 5 or n = 7:
-                    print("*POP*)
-
-count = 0 
-while count < 4:
-    count = count + 1
-
-current_band = {}
-
-class Band(object):
-    def __init__(self, name):
-        self.name = name
-
-    def bnd_add(current_band): # Adds member to band
-        for name in bandmates:
-            add = input(bandmates[name])
-            if add == 'y' or add == 'yes':
-                current_band[add] = add
-                print(current_band)
-
-    def bnd_sub(current_band): # Boots member from band
-        for name in current_band:
-            sub = input( "Who should we boot?" )
-            print(current_band)
-            if sub in current_band:
-                del name[sub]
-            else:
-                print( "Up to you" )
-            
-if __name__ == '__main__':
-    ch_affirm = ['y', 'yes']
-    bld_band = input( "Got any suggestions for a band?" )
-    for bld_band in ch_affirm:
-        bnd_add(current_band)
-        bnd_sub(current_band)
-            
-            
-ver 3.0 
-                          
-import random
-
-bandmates = {
-    "Kenneth": "Bangs a good drum, when he's sober. You sure?", 
-    "David": "Rocks the bass well, just gotta watch that temper. Cool?",
-    "Derek": "The staple of every band, he was the only one to bother showing up though. Yes?",
-    "Josie": "She can sing your head off, but can get carried away. You good?",
-}
 
 combust_chance = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
@@ -155,54 +18,39 @@ class Bassist(Musician): # The Musician class is the parent of the Bassist class
         super().__init__(name, ["Twang", "Thrumb", "Bling"])
 
     def tune(self):
-        print("Yeah, hold on.")
+        print("Yeah, hold on... still tuning...")
         print("Thuuum", "Duuum", "Buuum") 
         
-    def solo(self):
-        david.solo(5)
-        print(david.sounds)
-
 class Guitarist(Musician): # The Musician class is the parent of the Guitarist class
     def __init__(self, name):
         super().__init__(name, ["Boink", "Bow", "Boom"])
         
     def tune(self):
-        print("Be with you in a moment")
+        print("Be with you in a moment, tuning...")
         print("Twoning, sproing, splang")
-        
-    def solo(self):
-        derek.solo(9)
-        print(derek.sounds)
         
 class Vocalist(Musician): # The Musician class is the parent of the Vocalist class
     def __init__(self, name):
-        super().__init__(name, ["Oooh", "Ahhhh", "Eeeeee"])
+        super().__init__(name, ["Oooh!", "Ahhhh!", "Eeeeee!"])
         
     def tune(self):
-        print("Have to clear my throat.")
+        print("Have to clear my throat... AHEM!")
         print("Do", "Re", "Mi",)    
-        
-    def solo(self):
-        josie.solo(3)
-        print(josie.sounds)
-        
+  
 class Drummer(Musician): # The Musician class is the parent of the Drummer class
     def __init__(self, name):
         super().__init__(name, ["Badum", "Tish", "Crash"])
-    
-    def tune(self):
-        print("Let me key things up first...")
-        print("Smish, Smash, Brrt")
-    
-    def solo(self):
-        kenneth.solo(9)
-        print(kenneth.sounds)
-
+   
     def countup(self):
         count = 0 
         while count < 4:
             count = count + 1
-        
+            print(count)
+    
+    def tune(self):
+        print("Let me key things up first...")
+        print("Smish, Smash, Brrt")
+   
     def combust(self):
         for n in combust_chance:
             random.choice(n)
@@ -221,7 +69,7 @@ class Band(object):
     def bnd_add(self, added_bandmate): # Adds member to band
         if isinstance(added_bandmate, Musician):
             self.bandmate.append(added_bandmate)
-            print("They're onboard!")
+            print("One more onboard!")
 
     def bnd_sub(self, bandmate_name): # Boots member from band
         for added_bandmate in self.bandmate:
@@ -229,29 +77,25 @@ class Band(object):
                 self.bandmate.remove(added_bandmate)
                 print("They've up and left!")
                 
-    def performance(self): #  Plays the instruments and actions of the different roles
-        for bandmate in self.bandmate:
-            initiate = input( "Have Kenneth start the count?" )
-            if initiate == 'y' or initiate == 'yes':
-                try: 
-                    bandmate.countup()
-                    bandmate.tune() 
-                    bandmate.solo()
-                    bandmate.combust()
+    def bnd_performance(self):
+        for player in self.bandmate:
+                try:
+                    player.countup()
+                    player.solo(3)
+                    player.combust()
                 except:
-                    bandmate.solo()
-                    print("Maybe next time?")
+                    player.solo(3)
                     
-if __name__ == '__main__': 
-    print( "Let's meet the members of the up and coming band, 'Freak Beak'!" )
+if __name__ == '__main__':
+    print( "Meet the band,'Freak Beak'!" )
     Kenneth = Drummer("Kenneth")
     David = Bassist("David")
     Derek = Guitarist("Derek")
     Josie = Vocalist("Josie")
     Band = Band("Freak Beak")
-    Band.bnd_add(Kenneth) #adds Kenneth, the drummer
-    Band.bnd_add(David) #adds David, the bassist
-    Band.bnd_add(Derek) #adds Derek, the Guitarist
-    Band.bnd_add(Josie) #adds Josie, the Vocalist
-    Band.performance()
+    Band.bnd_add(Kenneth)
+    Band.bnd_add(David)
+    Band.bnd_add(Derek)
+    Band.bnd_add(Josie)
+    Band.bnd_performance()
     
